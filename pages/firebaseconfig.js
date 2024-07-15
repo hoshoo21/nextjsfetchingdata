712f09f995e5e330223ddd422e037a5b68c075f5
naveedasad21 <143587424+naveedasad21@users.noreply.github.com>
2024-07-15 07:18:17 -0700

    final commit

--- pages/firebaseconfig.js
+++ pages/firebaseconfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { firestore } from 'firebase/compat/firestore';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyD8xdwr72g5ebkxr5dQVCxfg0pl78mE4yw",
+    apiKey: "",
     authDomain: "reactproj-f9640.firebaseapp.com",
-    projectId: "reactproj-f9640",
-    storageBucket: "reactproj-f9640.appspot.com",
-    messagingSenderId: "598951432365",
-    appId: "1:598951432365:web:e1989f7f59283691f27624"
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const appconfig = initializeApp(firebaseConfig);
